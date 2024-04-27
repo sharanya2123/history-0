5a4d47b28fd46e9147e45a57731ef5851ef1b9ec
sharanya2123 <user.email>
2024-04-27 03:46:15 +0000

    history-0

---
+++ src/recomend/index.js
@@ -0,0 +1,45 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyDetails, onDeleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onRemoveHistory = () => {
+    onDeleteHistory(id)
+  }
+
+  return (
+    <li className="history-item-con1">
+      <div className="history-item-con2">
+        <p className="history-time">{timeAccessed}</p>
+
+        <div className="history-item-con3">
+          <img
+            src={logoUrl}
+            alt="domain logo"
+            className="history-domain-logo"
+          />
+
+          <div className="history-item-con4">
+            <p className="history-title">{title}</p>
+            <p className="history-domain-url">{domainUrl}</p>
+          </div>
+        </div>
+      </div>
+
+      <button
+        className="history-del-btn"
+        type="button"
+        onClick={onRemoveHistory}
+        data-testid="delete"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="history-del-icon"
+        />
+      </button>
+    </li>
+  )
+}
+export default HistoryItem
